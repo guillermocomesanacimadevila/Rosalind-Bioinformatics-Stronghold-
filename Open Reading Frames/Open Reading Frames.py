da360167702c74570697947a81fd50bd7977071b
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2024-12-27 20:24:26 +0100

    Update Open Reading Frames.py

--- Open Reading Frames/Open Reading Frames.py
+++ Open Reading Frames/Open Reading Frames.py
@@ -2,30 +2,33 @@
 
 def rev_comp(sequence):
     rev_comp_dict = {"A": "T", "T": "A", "G": "C", "C": "G"}
-    for nuc in range(len(sequence)):
-        sequence[nuc] = rev_comp_dict[sequence[nuc]]
-    return sequence [::-1]
+    return "".join(rev_comp_dict[nuc] for nuc in sequence[::-1])
 
 def transcription(sequence):
     return sequence.replace('T', 'U')
 
 def translation(sequence):
-    codon_protein_dictionary = {"AUG":"M","UUU":"F","UUC":"F",'UUA':'L','UUG':'L','UCU':'S',
-                  'UCC':'S','UCA':'S','UCG':'S','CUU': 'L','AUU': 'I','GUU': 'V',
-                  'CUC': 'L','AUC': 'I','GUC': 'V','CUA': 'L','AUA': 'I','GUA': 'V',
-                  'CUG': 'L','GUG': 'V','CCU': 'P', 'ACU': 'T','GCU': 'A','CCC': 'P',
-                  'ACC': 'T','GCC': 'A','CCA': 'P','ACA': 'T','GCA': 'A','CCG': 'P',
-                  'ACG': 'T','GCG': 'A','UAU': 'Y','CAU': 'H','AAU': 'N','GAU': 'D',
-                  'UAC': 'Y','CAC': 'H','AAC': 'N','GAC': 'D','UAA': '*','CAA': 'Q',
-                  'AAA': 'K','GAA': 'E','UAG': '*','CAG': 'Q','AAG': 'K','GAG': 'E',
-                  'UGU': 'C','CGU': 'R','AGU': 'S','GGU': 'G','UGC': 'C','CGC': 'R',
-                  'AGC': 'S','GGC': 'G','UGA': '*','CGA': 'R','AGA': 'R','GGA': 'G',
-                  'UGG': 'W','CGG': 'R','AGG': 'R','GGG': 'G'
-                                }
+    codon_protein_dictionary = {
+        "AUG": "M", "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+        "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "CUU": "L",
+        "AUU": "I", "GUU": "V", "CUC": "L", "AUC": "I", "GUC": "V",
+        "CUA": "L", "AUA": "I", "GUA": "V", "CUG": "L", "GUG": "V",
+        "CCU": "P", "ACU": "T", "GCU": "A", "CCC": "P", "ACC": "T",
+        "GCC": "A", "CCA": "P", "ACA": "T", "GCA": "A", "CCG": "P",
+        "ACG": "T", "GCG": "A", "UAU": "Y", "CAU": "H", "AAU": "N",
+        "GAU": "D", "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
+        "UAA": "*", "CAA": "Q", "AAA": "K", "GAA": "E", "UAG": "*",
+        "CAG": "Q", "AAG": "K", "GAG": "E", "UGU": "C", "CGU": "R",
+        "AGU": "S", "GGU": "G", "UGC": "C", "CGC": "R", "AGC": "S",
+        "GGC": "G", "UGA": "*", "CGA": "R", "AGA": "R", "GGA": "G",
+        "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+    }
     protein = ""
-    for nuc in range(0, len(sequence), 3):
+    for nuc in range(0, len(sequence) - 2, 3):
         codon = sequence[nuc:nuc+3]
         if codon in codon_protein_dictionary:
+            if codon_protein_dictionary[codon] == "*":
+                break
             protein += codon_protein_dictionary[codon]
     return protein
 
@@ -36,19 +39,15 @@ import os
 def read_file(location):
     with open(os.path.expanduser(location), "r") as f:
         file = f.readlines()
-        tags = ""
         sequence = ""
         for line in file:
-            if line.startswith(">"):
-                tags += line.strip()
-            else:
-                sequence += line.replace("\n", "")
+            if not line.startswith(">"):
+                sequence += line.strip()
         return sequence
 
-orf_file = translation(transcription(read_file("~/Desktop/rosalind_orf.txt")))
-orf_file_rna = transcription(read_file("~/Desktop/rosalind_orf.txt"))
-orf_file_rna_rev = transcription(rev_comp(orf_file_rna))
-print(orf_file)
+dna_sequence = read_file("~/Desktop/rosalind_orf.txt")
+orf_file_rna = transcription(dna_sequence)
+orf_file_rna_rev = transcription(rev_comp(dna_sequence))
 
 genes = []
 stop_codons = ["UAG", "UAA", "UGA"]
@@ -66,9 +65,8 @@ for nuc in range(0, len(orf_file_rna) - 2, 3):
             genes.append(translation(gene))
 
 genes_by_lines = "\n".join(genes)
-print(genes_by_lines)
+# print(genes_by_lines)
 
-# Second loop, forgot to make it into a function
 genes_rev = []
 for nucleotide in range(0, len(orf_file_rna_rev) - 2, 3):
     codon_1 = orf_file_rna_rev[nucleotide:nucleotide+3]
